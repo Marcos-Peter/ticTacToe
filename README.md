04318c312c9e7064bbf86601e9e31eed2c6f64a9
Marcos Peter <mpeterlobato@gmail.com>
2021-06-20 18:06:23 -0300

    Website structure, ticTacToe first steps

---
+++ README.md
@@ -0,0 +1,6 @@
+# :man_technologist: WELCOME TO MY FIRST WEBSITE :man_technologist:
+
+##### 	I'm starting with my web dev studies and have the idea to make a website to share somethings that I'm learning.
+
+###### As soon I implementing something new I'll push here and try to make it online.
+
---
+++ js/portifolio.js
@@ -0,0 +1,89 @@
+// flip the main card
+const card = document.querySelector("#card");
+card.addEventListener("click", (event) => {
+  if(event.target.matches('#front'))
+  {
+    card.classList.toggle("flip");
+  } else if(event.target.matches('.cell'))
+  {
+    logic(event.target.id);
+  }
+});
+
+// The game Logic
+// Getting all the cells of the html document
+const cells = document.querySelectorAll('.cell');
+let shiftCheck = true;
+
+let playerOne = 'X';
+let playerTwo = 'O';
+
+const conditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+// Add an action at the click and the arrow function make the test for the cells.
+// document.addEventListener('click', (event) => {
+//     if(event.target.matches('.cell'))
+//     {
+//         logic(event.target.id);
+//     }
+// });
+
+function logic(id) {
+    const cell = document.getElementById(id);
+    shift = shiftCheck ? playerOne : playerTwo;
+    cell.textContent = shift;
+    cell.classList.add(shift);
+    winnerCheck(shift);
+}
+
+function winnerCheck(shift) {
+
+    const winner = conditions.some((cond) => {
+        return cond.every((index) => {
+            return cells[index].classList.contains(shift);
+        })
+    });
+
+    if(winner) {
+        gameOver(shift);
+    } else if(tieCheck()) {
+        gameOver();
+    } else {
+        shiftCheck = !shiftCheck;
+    }
+}
+
+function tieCheck() {
+    let x = 0;
+    let o = 0;
+
+    for(index in cells) {
+        if(!isNaN(index)) {
+            if(cells[index].classList.contains(playerOne)) {
+                x++;
+            }
+            if(cells[index].classList.contains(playerTwo)) {
+                o++;
+            }
+        }
+    }
+
+    return x + o === 9 ? true : false;
+}
+
+function gameOver(winner = null) {
+    if(winner) {
+        console.log("Winner: "+winner);
+    } else {
+        console.log("We have a tie!");
+    }
+}
